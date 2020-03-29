e9dcd3fba81605cdfc728d63ba00b2c84e33f0c0
ABAdkins <a.brown46464@gmail.com>
2020-03-29 17:18:55 -0500

    made permissions a bit better

--- app/src/main/java/com/example/newoptinav/MainActivity.java
+++ app/src/main/java/com/example/newoptinav/MainActivity.java
@@ -2,7 +2,16 @@ package com.example.newoptinav;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void checkPermission(View view) {
+        PermissionListener permissionListener = new PermissionListener() {
+            @Override
+            public void onPermissionGranted() {
+                startActivity(new Intent(MainActivity.this, MapsActivity.class));
+            }
+
+            @Override
+            public void onPermissionDenied(List<String> deniedPermissions) {
+                Toast.makeText(MainActivity.this, "permission denied", Toast.LENGTH_SHORT).show();
+            }
+        };
+        //ask for permission
+        TedPermission.with(MainActivity.this)
+                .setPermissionListener(permissionListener)
+                .setPermissions(Manifest.permission.ACCESS_FINE_LOCATION)
+                .check();
     }
 }
